750aa47eda1f6e6e2cc821b2c521516e5984e884
khanlarazizov <xanlar.ezizov18@gmail.com>
2024-09-15 02:19:36 +0400

    register-user-controller-added

---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Events\UserRegistered;
+use App\Helpers\ResponseHelper;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisteredUserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RegisteredUserController extends Controller
+{
+    public function store(RegisteredUserRequest $request)
+    {
+        $input = $request->validated();
+        $input['password'] = Hash::make($request->input('password'));
+
+        $user = User::create($input);
+        $user->assignRole('user');
+
+        if (!$user)
+            return ResponseHelper::error(
+                message: 'User not registered',
+                statusCode: 400
+            );
+
+        event(new UserRegistered($user));
+//        Notification::send($user, new WelcomeNewUser($user));
+
+        return ResponseHelper::success(
+            message: 'User registered succesfully',
+            data: new UserResource($user),
+            statusCode: 201
+        );
+    }
+}
