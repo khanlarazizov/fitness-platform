480b8e47739382e321584c39d842bc8504a75cdc
khanlarazizov <xanlar.ezizov18@gmail.com>
2024-09-15 02:18:41 +0400

    workout-added

---
+++ app/Lib/Interfaces/IWorkoutRepository.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Lib\Interfaces;
+
+use App\Models\Workout;
+use Illuminate\Support\Collection;
+
+interface IWorkoutRepository
+{
+    public function getAllWorkouts(): Collection;
+
+    public function getWorkoutById(int $id): ?Workout;
+
+    public function createWorkout(array $data): Workout;
+
+    public function updateWorkout(int $id, array $data): bool;
+
+    public function deleteWorkout(int $id);
+}
