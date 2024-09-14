c2fa9e453036b30b524c8bae3cea2115a42fac7f
khanlarazizov <xanlar.ezizov18@gmail.com>
2024-09-15 02:22:02 +0400

    user-login-request-updated

--- app/Http/Requests/Auth/UserLoginRequest.php
+++ app/Http/Requests/Auth/UserLoginRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests\Auth;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
-class LoginUserRequest extends FormRequest
+class UserLoginRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -23,7 +23,7 @@ class LoginUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'email' => ['required', 'email', Rule::exists('users','email')],
+            'email' => ['required', 'email', Rule::exists('users', 'email'), 'max:20'],
             'password' => ['required'],
         ];
     }
