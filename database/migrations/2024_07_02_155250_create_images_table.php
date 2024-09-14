a885d7c45e59546013579a02d43fa52bdc092a05
khanlarazizov <xanlar.ezizov18@gmail.com>
2024-09-15 02:14:16 +0400

    image-table-added

--- database/migrations/2024_07_02_155250_create_images_table.php
+++ database/migrations/2024_07_02_155250_create_images_table.php
@@ -13,7 +13,9 @@ return new class extends Migration {
         Schema::create('images', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+//            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->unsignedBigInteger('imageable_id');
+            $table->string('imageable_type');
             $table->timestamps();
             $table->softDeletes();
         });
