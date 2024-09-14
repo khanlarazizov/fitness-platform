bd316f65b7677a5227665a29991801a24b5d9464
khanlarazizov <xanlar.ezizov18@gmail.com>
2024-09-15 02:21:10 +0400

    user-seeder-updated

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -22,37 +22,5 @@ class UserSeeder extends Seeder
         $users->each(function ($user) {
             $user->assignRole('user');
         });
-
-//        $user = User::create([
-//            'name' => 'Akif',
-//            'surname' => 'Akif',
-//            'email' => 'admin@gmail.com',
-//            'password' => Hash::make('adminadmin'),
-//            'email_verified_at' => now(),
-//            'phone_number' => '+99455' . rand(1111111, 9999999),
-//            'status' => UserStatusEnum::ACTIVE->value,
-//            'trainer_id' => null
-//        ]);
-//
-//        $user->assignRole('admin');
-//        $role = Role::find(1);
-//        $permissions = Permission::all();
-//        $role->syncPermissions($permissions);
-
-//        $user = User::create([
-//            'name' => 'trainer',
-//            'surname' => 'trainer',
-//            'email' => 'trainer@gmail.com',
-//            'password' => Hash::make('trainer'),
-//            'email_verified_at' => now(),
-//            'phone_number' => '+99455' . rand(1111111, 9999999),
-//            'status' => UserStatusEnum::ACTIVE->value,
-//            'trainer_id' => null
-//        ]);
-//
-//        $user->assignRole('trainer');
-//        $role = Role::find(2);
-//        $permissions = Permission::all();
-//        $role->syncPermissions($permissions);
     }
 }
