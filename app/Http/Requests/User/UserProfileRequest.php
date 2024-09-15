36f5fe016fb51a8e8c2f57a040f37978d96ba886
khanlarazizov <xanlar.ezizov18@gmail.com>
2024-09-16 02:03:59 +0400

    user-profile-request-added

---
+++ app/Http/Requests/User/UserProfileRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use App\Enums\StatusEnum;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Enum;
+use Illuminate\Validation\Rules\Password;
+
+class UserProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'min:3', 'max:255', 'regex:/(^([a-zA-Z]+)(\d+)?$)/u'],
+            'surname' => ['required', 'min:3', 'max:255'],
+            'email' => ['required', 'email', Rule::unique('users', 'email')
+                ->ignore($this->user, 'id')
+                ->whereNull('deleted_at')
+            ],
+            'phone_number' => [
+                'required',
+                'regex:/^(\+994|994|0)(50|51|55|60|70|77)\d{7}$/',
+                'min:9',
+                'max:15',
+                Rule::unique('users', 'phone_number')
+                    ->ignore($this->user, 'id')
+                    ->whereNull('deleted_at')],
+            'status' => ['required', new Enum(StatusEnum::class)],
+            'trainer_id' => ['sometimes', 'exists:users,id'],
+            'file' => ['nullable', 'mimes:jpg,jpeg,png,txt', 'max:2048'],
+        ];
+    }
+}
