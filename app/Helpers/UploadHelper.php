a1f6f9b9de9597fe297241663d12f12964fcb3be
khanlarazizov <xanlar.ezizov18@gmail.com>
2024-09-16 01:56:29 +0400

    upload-helper-updated

--- app/Helpers/UploadHelper.php
+++ app/Helpers/UploadHelper.php
@@ -23,20 +23,32 @@ class UploadHelper
         }
     }
 
-    public function updateFile($file, $modelName)
+    public static function updateFile($file, $model)
     {
         $year = now()->format('Y');
         $month = now()->format('m');
         $fileName = "";
+
         try {
             $fileName = Str::uuid() . '.' . $file->getClientOriginalExtension();
+
             $file->storeAs('uploads/' . $year . '/' . $month . '/', $fileName, 'public');
-            if ($modelName->image->fullName) {
-                Storage::delete('uploads/' . $year . '/' . $month . '/' . $modelName->image->fullName);
+
+            if ($model->image && $model->image->name) {
+                Storage::disk('public')->delete('uploads/' . $year . '/' . $month . '/' . $model->image->name);
             }
-        }catch (\Exception $e) {
+
+            return $fileName;
+        } catch (\Exception $e) {
             Log::error('Image could not be uploaded', ['error' => $e->getMessage()]);
             return null;
+        }
+    }
+
+    public static function deleteFile($model)
+    {
+        if ($model->image && $model->image->name) {
+            Storage::disk('public')->delete($model->image->name);
         }
     }
 }
