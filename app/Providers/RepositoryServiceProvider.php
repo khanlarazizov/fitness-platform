87de81073fdd037ad6f48ac59ef07da7fc402a4d
khanlarazizov <xanlar.ezizov18@gmail.com>
2024-09-16 01:54:13 +0400

    repository-service-provider--updated

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use App\Lib\CategoryRepository;
+use App\Lib\Interfaces\ICategoryRepository;
+use App\Lib\Interfaces\IPlanRepository;
+use App\Lib\Interfaces\IWorkoutRepository;
+use App\Lib\PlanRepository;
+use App\Lib\WorkoutRepository;
 use Illuminate\Support\ServiceProvider;
 use App\Lib\Interfaces\IUserRepository;
 use App\Lib\UserRepository;
@@ -14,6 +20,9 @@ class RepositoryServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(IUserRepository::class, UserRepository::class);
+        $this->app->bind(ICategoryRepository::class, CategoryRepository::class);
+        $this->app->bind(IWorkoutRepository::class, WorkoutRepository::class);
+        $this->app->bind(IPlanRepository::class, PlanRepository::class);
     }
 
     /**
