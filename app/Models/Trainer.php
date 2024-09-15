5ae66e19f2e0a36ebaa032a5110863736dfa7e74
khanlarazizov <xanlar.ezizov18@gmail.com>
2024-09-16 01:57:13 +0400

    trainer-model-updated

--- app/Models/Trainer.php
+++ app/Models/Trainer.php
@@ -2,8 +2,12 @@
 
 namespace App\Models;
 
+use App\Enums\GenderEnum;
+use App\Enums\StatusEnum;
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -12,7 +16,7 @@ use Spatie\Permission\Traits\HasRoles;
 
 class Trainer extends Authenticatable
 {
-    use HasFactory, Notifiable, SoftDeletes, HasRoles, HasApiTokens;
+    use HasFactory, Notifiable, SoftDeletes, HasRoles, HasApiTokens, Sluggable;
 
     protected $guard_name = 'trainer-api';
 
@@ -22,4 +26,34 @@ class Trainer extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    protected $casts = [
+        'status' => StatusEnum::class,
+        'gender' => GenderEnum::class,
+    ];
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => ['name', 'surname'],
+                'onUpdate' => true,
+            ]
+        ];
+    }
+
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class);
+    }
+
+    public function plans(): HasMany
+    {
+        return $this->hasMany(Plan::class);
+    }
+
+    public function image(): MorphOne
+    {
+        return $this->morphOne(Image::class, 'imageable');
+    }
 }
