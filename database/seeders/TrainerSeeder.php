95d4de80c2b0af994490390c31cc5e782c08228d
khanlarazizov <xanlar.ezizov18@gmail.com>
2024-09-16 01:57:46 +0400

    trainer-seeder-updated

--- database/seeders/TrainerSeeder.php
+++ database/seeders/TrainerSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Enums\GenderEnum;
+use App\Enums\StatusEnum;
 use App\Models\Trainer;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,14 +18,24 @@ class TrainerSeeder extends Seeder
      */
     public function run(): void
     {
-        $traine = Trainer::create([
+        $trainer = Trainer::factory(30)->create();
+
+        $trainer->each(
+            fn($trainer) => $trainer->assignRole('trainer')
+        );
+
+        $trainer2 = Trainer::create([
             'name' => 'trainer',
             'surname' => 'trainer',
             'email' => 'trainer@gmail.com',
             'password' => Hash::make('trainertrainer'),
+            'phone_number' => '+994555555555',
+            'gender' => GenderEnum::MALE->value,
+            'birth_date' => '1990-01-01',
+            'status' => StatusEnum::ACTIVE->value,
         ]);
 
-        $traine->assignRole('trainer');
+        $trainer2->assignRole('trainer');
         $role = Role::findByName('trainer', 'trainer-api');
         $trainerPermissions = Permission::where('guard_name', 'trainer-api')->get();
         $role->syncPermissions($trainerPermissions);
