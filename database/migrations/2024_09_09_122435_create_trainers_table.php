7066b42e7eda60ecdab4e1df3c9cca597714eee7
khanlarazizov <xanlar.ezizov18@gmail.com>
2024-09-16 01:45:44 +0400

    trainer-table-updated

--- database/migrations/2024_09_09_122435_create_trainers_table.php
+++ database/migrations/2024_09_09_122435_create_trainers_table.php
@@ -3,9 +3,10 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Enums\StatusEnum;
+use App\Enums\GenderEnum;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,11 +16,18 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('surname');
-            $table->string('email')->unique();
+            $table->string('slug');
+            $table->string('email');
+            $table->string('phone_number');
             $table->string('password');
+            $table->date('birth_date');
+            $table->enum('status', array_column(StatusEnum::cases(), 'value'))->default(StatusEnum::ACTIVE->value);
+            $table->enum('gender', array_column(GenderEnum::cases(), 'value'));
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
+
+            $table->unique(['slug', 'email', 'phone_number', 'deleted_at']);
         });
     }
 
