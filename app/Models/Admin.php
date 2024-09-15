1e70317c3547358c2e9b7f2627cba4b5f15c2368
khanlarazizov <xanlar.ezizov18@gmail.com>
2024-09-16 01:46:49 +0400

    admin-model-updated

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -12,7 +14,7 @@ use Spatie\Permission\Traits\HasRoles;
 
 class Admin extends Authenticatable
 {
-    use HasFactory, Notifiable, SoftDeletes, HasRoles, HasApiTokens;
+    use HasFactory, Notifiable, SoftDeletes, HasRoles, HasApiTokens,Sluggable;
 
     protected $guard_name = 'admin-api';
 
@@ -22,4 +24,19 @@ class Admin extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => ['name', 'surname'],
+                'onUpdate' => true,
+            ]
+        ];
+    }
+
+    public function image(): MorphOne
+    {
+        return $this->morphOne(Image::class, 'imageable');
+    }
 }
