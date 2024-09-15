6ae0143bc10841bac59334e5d4c556adacbb05aa
khanlarazizov <xanlar.ezizov18@gmail.com>
2024-09-16 01:58:29 +0400

    workout-resource-updated

--- app/Http/Resources/WorkoutResource.php
+++ app/Http/Resources/WorkoutResource.php
@@ -17,6 +17,7 @@ class WorkoutResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
+            'category' => new CategoryResource($this->whenLoaded('category')),
             'image' => new ImageResource($this->whenLoaded('image')),
         ];
     }
