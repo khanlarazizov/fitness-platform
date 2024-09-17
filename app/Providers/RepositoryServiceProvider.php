a80c4643f4e98f9fed9312668a1bfe9ac9cd4cf8
khanlarazizov <xanlar.ezizov18@gmail.com>
2024-09-18 01:45:31 +0400

    trainer-repository-added-to-provider

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -5,8 +5,10 @@ namespace App\Providers;
 use App\Lib\CategoryRepository;
 use App\Lib\Interfaces\ICategoryRepository;
 use App\Lib\Interfaces\IPlanRepository;
+use App\Lib\Interfaces\ITrainerRepository;
 use App\Lib\Interfaces\IWorkoutRepository;
 use App\Lib\PlanRepository;
+use App\Lib\TrainerRepository;
 use App\Lib\WorkoutRepository;
 use Illuminate\Support\ServiceProvider;
 use App\Lib\Interfaces\IUserRepository;
@@ -23,6 +25,7 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(ICategoryRepository::class, CategoryRepository::class);
         $this->app->bind(IWorkoutRepository::class, WorkoutRepository::class);
         $this->app->bind(IPlanRepository::class, PlanRepository::class);
+        $this->app->bind(ITrainerRepository::class, TrainerRepository::class);
     }
 
     /**
