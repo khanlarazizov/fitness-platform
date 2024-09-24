1db99c53c4607398744be85b0526b1636a017b47
khanlarazizov <xanlar.ezizov18@gmail.com>
2024-09-24 12:58:43 +0400

    plan-seeder-updated

--- database/seeders/PlanSeeder.php
+++ database/seeders/PlanSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Plan;
+use App\Models\User;
 use App\Models\Workout;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
