f2fa0e177b4816b27124d898d0e27a2ef2d74cb9
khanlarazizov <xanlar.ezizov18@gmail.com>
2024-09-24 12:56:00 +0400

    workout-table-updated

--- database/migrations/2024_09_09_212409_create_workouts_table.php
+++ database/migrations/2024_09_09_212409_create_workouts_table.php
@@ -15,6 +15,7 @@ return new class extends Migration {
             $table->string('name');
             $table->string('slug');
             $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->text('description')->nullable();
             $table->softDeletes();
             $table->timestamps();
 
