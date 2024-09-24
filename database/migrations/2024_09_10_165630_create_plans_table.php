0065e96dfddbee0b4f4112a7555d3117ba94ef2f
khanlarazizov <xanlar.ezizov18@gmail.com>
2024-09-24 12:56:33 +0400

    plans-table-updated

--- database/migrations/2024_09_10_165630_create_plans_table.php
+++ database/migrations/2024_09_10_165630_create_plans_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Enums\StatusEnum;
 
 return new class extends Migration {
     /**
@@ -14,8 +15,9 @@ return new class extends Migration {
             $table->id();
             $table->string('name');
             $table->string('slug');
-//            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('trainer_id')->constrained()->cascadeOnDelete();
+            $table->enum('status',array_column(StatusEnum::cases(),'value'))->default(StatusEnum::ACTIVE->value);
+            $table->foreignId('trainer_id')->constrained('users');
+            $table->text('description')->nullable();
             $table->softDeletes();
             $table->timestamps();
         });
