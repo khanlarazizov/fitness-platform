5806d0b6cc09c8c1d82f24a1c97ed122175ca9e1
khanlarazizov <xanlar.ezizov18@gmail.com>
2024-09-24 13:10:17 +0400

    permission-seeder-updated

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -2,47 +2,24 @@
 
 namespace Database\Seeders;
 
+use App\Enums\PermissionEnum;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
 
 class PermissionSeeder extends Seeder
 {
-    public array $permissions = [
-        'index', 'store', 'show', 'update', 'destroy'
-    ];
-
-    public array $adminControllers = [
-        'admin',
-        'user',
-        'trainer',
-        'workout',
-        'plan',
-        'category',
-        'role',
-        'permission'
-    ];
-
-    public array $trainerControllers = [
-        'workout',
-        'plan',
-        'category',
-    ];
+//    public array $permissions = [
+//        'index', 'store', 'show', 'update', 'destroy'
+//    ];
+//    public array $controllers = [
+//        'user', 'role', 'permission', 'workout', 'category', 'trainer', 'plan'
+//    ];
 
     public function run(): void
     {
-        // Admin Permissions
-        foreach ($this->adminControllers as $controller) {
-            foreach ($this->permissions as $permission) {
-                Permission::create(['name' => $controller . '-' . $permission, 'guard_name' => 'admin-api']);
-            }
-        }
-
-        // TrainerController Permissions
-        foreach ($this->trainerControllers as $controller) {
-            foreach ($this->permissions as $permission) {
-                Permission::create(['name' => $controller . '-' . $permission, 'guard_name' => 'trainer-api']);
-            }
+        foreach (PermissionEnum::cases() as $permission) {
+            Permission::create(['name' => $permission->label(), 'guard_name' => 'api']);
         }
     }
 }
