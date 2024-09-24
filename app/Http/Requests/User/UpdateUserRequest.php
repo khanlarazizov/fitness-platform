f964b5cd90eb49cdd344eaaceb29fd2754b9f333
khanlarazizov <xanlar.ezizov18@gmail.com>
2024-09-24 13:02:39 +0400

    update-user-request-updated

--- app/Http/Requests/User/UpdateUserRequest.php
+++ app/Http/Requests/User/UpdateUserRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\User;
 
+use App\Enums\RoleEnum;
 use App\Enums\StatusEnum;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
@@ -28,26 +29,16 @@ class UpdateUserRequest extends FormRequest
         return [
             'name' => ['required', 'min:3', 'max:255', 'regex:/(^([a-zA-Z]+)(\d+)?$)/u'],
             'surname' => ['required', 'min:3', 'max:255'],
-            'email' => ['required', 'email', Rule::unique('users', 'email')
-                ->ignore($this->user, 'id')
-                ->whereNull('deleted_at')
-            ],
-            'password' => ['required', 'confirmed',
-                Password::min(6)
-                    ->max(20)
-                    ->numbers()
-                    ->uncompromised()],
-            'phone_number' => [
-                'required',
-                'regex:/^(\+994|994|0)(50|51|55|60|70|77)\d{7}$/',
-                'min:9',
-                'max:15',
-                Rule::unique('users', 'phone_number')
-                    ->ignore($this->user, 'id')
-                    ->whereNull('deleted_at')],
+            'email' => ['required', 'email', Rule::unique('users', 'email')->ignore($this->user, 'id')->whereNull('deleted_at')],
+            'password' => ['required', 'confirmed', Password::min(6)->max(20)->numbers()],
+            'phone_number' => ['required', 'regex:/^(\+994|994|0)(50|51|55|60|70|77)\d{7}$/', 'min:9', 'max:15', Rule::unique('users', 'phone_number')->ignore($this->user, 'id')->whereNull('deleted_at')],
             'status' => ['required', new Enum(StatusEnum::class)],
             'trainer_id' => ['sometimes', 'exists:users,id'],
-            'file' => ['nullable', 'mimes:jpg,jpeg,png,txt', 'max:2048'],
+            'file' => ['nullable', 'mimes:jpg,jpeg,png', 'max:2048'],
+            'role' => ['required', new Enum(RoleEnum::class)],
+            'weight' => ['required_unless:role,' . RoleEnum::ADMIN->value, 'numeric'],
+            'height' => ['required_unless:role,' . RoleEnum::ADMIN->value, 'numeric'],
+            'about' => ['required_if:role,' . RoleEnum::TRAINER->value, 'string'],
         ];
     }
 }
