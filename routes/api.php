898b6ced56b08fed9010956240b2ef73d295e6ff
khanlarazizov <xanlar.ezizov18@gmail.com>
2024-11-11 00:43:11 +0400

    api-routes-updated

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,10 @@
 
 use App\Http\Controllers\API\CategoryController;
 use App\Http\Controllers\API\Plan\AssignPlan;
-use App\Http\Controllers\API\Plan\AssignPlanByTrainer;
 use App\Http\Controllers\API\Plan\PlanController;
+use App\Http\Controllers\API\Plan\TrainerPlanController;
+use App\Http\Controllers\API\User\ChooseTrainer;
+use App\Http\Controllers\API\User\TrainerStudentController;
 use App\Http\Controllers\API\User\UserController;
 use App\Http\Controllers\API\User\UserProfileController;
 use App\Http\Controllers\API\WorkoutController;
@@ -13,7 +15,10 @@ use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [UserController::class, 'index']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/posts');
+});
+
 Route::prefix('auth')->group(function () {
     Route::post('login', LoginController::class);
     Route::post('logout', LogoutController::class)->middleware('auth:sanctum');
@@ -23,30 +28,35 @@ Route::prefix('auth')->group(function () {
 });
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::prefix('profile')
-        ->controller(UserProfileController::class)
-        ->group(function () {
-            Route::get('/', 'show');
-            Route::put('/', 'update');
-        });
+    Route::put('choose-trainer/{id}', [ChooseTrainer::class, 'store']);
+    Route::delete('cancel-trainer', [ChooseTrainer::class, 'destroy']);
+
+    Route::get('profile/', [UserProfileController::class, 'show']);
+    Route::put('profile/', [UserProfileController::class, 'update']);
 
     Route::middleware('role:admin')->group(function () {
         Route::apiResource('users', UserController::class);
+        Route::apiResource('plans', PlanController::class);
     });
 
     Route::middleware('admin_or_trainer')->group(function () {
         Route::apiResource('workouts', WorkoutController::class);
         Route::apiResource('categories', CategoryController::class);
-        Route::apiResource('plans', PlanController::class);
     });
 
-    Route::post('assign-plan/{plan}', [AssignPlan::class, 'store']);
-    Route::delete('cancel-plan/{plan}', [AssignPlan::class, 'destroy']);
-
     Route::middleware('role:trainer')
-        ->controller(AssignPlanByTrainer::class)
         ->group(function () {
-            Route::post('assign-plan-by-trainer/{plan}', 'store');
-            Route::delete('cancel-plan-by-trainer/{plan}', 'destroy');
+            Route::apiResource('my-plans', TrainerPlanController::class);
+
+            Route::prefix('my-students')
+                ->controller(TrainerStudentController::class)
+                ->group(function () {
+                    Route::get('/', 'index');
+                    Route::get('{user}', 'show');
+                    Route::delete('{user}', 'destroy');
+                });
+
+            Route::post('assign-plan/{plan}', [AssignPlan::class, 'store']);
+            Route::delete('cancel-plan/{plan}', [AssignPlan::class, 'destroy']);
         });
 });
