80ca1b25882b75d624c2c693eb2b05dbc2be4f00
khanlarazizov <xanlar.ezizov18@gmail.com>
2024-11-11 00:46:40 +0400

    permission-seeder-updated

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -9,17 +9,10 @@ use Spatie\Permission\Models\Permission;
 
 class PermissionSeeder extends Seeder
 {
-//    public array $permissions = [
-//        'index', 'store', 'show', 'update', 'destroy'
-//    ];
-//    public array $controllers = [
-//        'user', 'role', 'permission', 'workout', 'category', 'trainer', 'plan'
-//    ];
-
     public function run(): void
     {
         foreach (PermissionEnum::cases() as $permission) {
-            Permission::create(['name' => $permission->label(), 'guard_name' => 'api']);
+            Permission::create(['name' => $permission, 'guard_name' => 'api']);
         }
     }
 }
