e3b9a90b22269f57adf05cd2eb9655862361d1f4
khanlarazizov <xanlar.ezizov18@gmail.com>
2024-11-11 01:08:41 +0400

    response-helper-updated

--- app/Helpers/ResponseHelper.php
+++ app/Helpers/ResponseHelper.php
@@ -6,19 +6,18 @@ use Illuminate\Http\JsonResponse;
 
 class ResponseHelper
 {
-    public static function success($status = 'success', $message = null, $data = [], $statusCode = 200): JsonResponse
+    public static function success($message = null, $data = [], $statusCode = 200): JsonResponse
     {
         return response()->json([
-            'status' => $status,
-            'message' => $message,
+            'success' => true,
             'data' => $data
         ], $statusCode);
     }
 
-    public static function error($status = 'error', $message = null, $statusCode = 404): JsonResponse
+    public static function error($message = null, $statusCode = 404): JsonResponse
     {
         return response()->json([
-            'status' => $status,
+            'status' => false,
             'message' => $message
         ], $statusCode);
     }
