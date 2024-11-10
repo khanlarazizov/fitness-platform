f8f6b1655d8d5809ae600c3b991e371d588f17c8
khanlarazizov <xanlar.ezizov18@gmail.com>
2024-11-11 01:07:10 +0400

    telescope-added

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,5 +4,6 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\EventServiceProvider::class,
     App\Providers\RepositoryServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
     Spatie\Permission\PermissionServiceProvider::class,
 ];
