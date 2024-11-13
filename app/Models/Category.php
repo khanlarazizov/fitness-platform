824971834cf00bc0e61d4182f9c771fc9ca507e9
khanlarazizov <xanlar.ezizov18@gmail.com>
2024-11-13 20:49:29 +0400

    category-model-updated

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
+use App\Enums\DirectionEnum;
 use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -11,7 +13,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
-    use HasFactory,SoftDeletes, Sluggable;
+    use HasFactory, SoftDeletes, Sluggable;
 
     public function sluggable(): array
     {
@@ -24,6 +26,20 @@ class Category extends Model
     }
 
     protected $guarded = ['id', 'created_at', 'updated_at', 'deleted_at'];
+
+    public function scopeName(Builder $query, $name)
+    {
+        if (!is_null($name)) {
+            return $query->where('name', 'like', '%' . $name . '%');
+        }
+    }
+
+    public function scopeSortBy(Builder $query, $sortBy, $direction = DirectionEnum::ASC->value)
+    {
+        if (in_array($sortBy, ['name', 'created_at']) && in_array($direction, array_column(DirectionEnum::cases(), 'value'))) {
+            return $query->orderBy($sortBy, $direction);
+        }
+    }
 
     public function image(): MorphOne
     {
