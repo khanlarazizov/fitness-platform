492dc88cad788003370f929642872fa339f09d1c
khanlarazizov <xanlar.ezizov18@gmail.com>
2024-11-14 11:59:44 +0400

    filter-workout-updated

--- app/Models/Workout.php
+++ app/Models/Workout.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
+use App\Enums\DirectionEnum;
 use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -25,6 +27,29 @@ class Workout extends Model
     }
 
     protected $guarded = ['id', 'created_at', 'updated_at', 'deleted_at'];
+
+    public function scopeName(Builder $query, $name)
+    {
+        if (!is_null($name)) {
+            return $query->where('name', 'like', '%' . $name . '%');
+        }
+    }
+
+    public function scopeCategory(Builder $query, $category_id)
+    {
+        if (!is_null($category_id)) {
+            return $query->whereHas('category', fn($query) => $query->where('id', $category_id));
+        }
+    }
+
+    public function scopeSortBy(Builder $query, $sortBy, $direction = DirectionEnum::ASC->value)
+    {
+        if (in_array($sortBy, ['name', 'created_at'])
+            && in_array($direction, array_column(DirectionEnum::cases(), 'value'))
+        ) {
+            return $query->orderBy($sortBy, $direction);
+        }
+    }
 
     public function image(): MorphOne
     {
