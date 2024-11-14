3b3c018ab3f35887159344c199044292f1bab99d
khanlarazizov <xanlar.ezizov18@gmail.com>
2024-11-14 11:53:17 +0400

    search-plan-request-added

---
+++ app/Http/Requests/Category/SearchPlanRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Category;
+
+use App\Enums\DirectionEnum;
+use App\Enums\StatusEnum;
+use App\Rules\TrainerExists;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Enum;
+
+class SearchPlanRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['nullable', 'string', 'max:25'],
+            'sort_by' => ['nullable', 'string', 'max:25'],
+            'status' => ['nullable', new Enum(StatusEnum::class)],
+            'trainer_id' => ['nullable', new TrainerExists()],
+            'direction' => ['nullable', new Enum(DirectionEnum::class)],
+        ];
+    }
+}
